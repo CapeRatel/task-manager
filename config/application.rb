519c807090698ef4f3073dc1b34cc0fb75088c88
Mikhail Krassovskiy <caperatel@MacBook-Pro-Mikhail.local>
2017-07-16 18:10:02 +0600

    config and gems for app

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,23 @@ module TaskManager
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.assets = false
+      g.helper = false
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_spec: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
